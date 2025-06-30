d595694487291d6aac2ca052a8476ada9ddc415e
Steven Lazaro <sslc.247@gmail.com>
2025-06-29 20:04:09 -0500

    v 1.1: Se agregaron vistas contacto y nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { ProductListComponent } from './public/pages/product-list/product-list';
+import { NosotrosComponent } from './public/pages/nosotros/nosotros';
+import { ContactoComponent } from './public/pages/contacto/contacto';
 
 export const routes: Routes = [
-    {
-        // Cuando la URL sea la raíz (ej: http://localhost:4200)
-        path: '', 
-        // Carga el componente de la lista de productos
-        component: ProductListComponent 
-    },
-    // Es una buena práctica añadir una ruta "comodín" para redirigir
-    // cualquier URL no encontrada a la página principal.
-    {
-        path: '**',
-        redirectTo: ''
-    }
+   
+
+    { path: '', component: ProductListComponent }, // Página de inicio
+    { path: 'catalogo', component: ProductListComponent }, // Ruta explícita
+    { path: 'nosotros', component: NosotrosComponent }, // Nueva ruta
+    { path: 'contacto', component: ContactoComponent }, // Nueva ruta
+    { path: '**', redirectTo: '' }
 ];
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router'; 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive], 
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  // Esta variable controlará si el menú móvil está visible o no.
+  // Por defecto, está cerrado (false).
+  isMobileMenuOpen = false;
 
+  // Esta función es llamada por el botón de hamburguesa en el HTML.
+  // Simplemente invierte el valor de isMobileMenuOpen.
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
