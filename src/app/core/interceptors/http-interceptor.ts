5d1eda4d6b9f21baa28ec35ae1e18b59e669734c
Steven Lazaro <sslc.247@gmail.com>
2025-07-19 18:29:21 -0500

    Cambios para deploy Google. se agrega cloudbuild

--- src/app/core/interceptors/http-interceptor.ts
+++ src/app/core/interceptors/http-interceptor.ts
@@ -2,20 +2,26 @@ import { inject } from '@angular/core';
 import { HttpEvent, HttpInterceptorFn, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StorageService } from '../services/storage.service';
+import { environment } from '../../../environments/environment';
 
 export const HttpRequestInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
  
   const storageService = inject(StorageService);
-  let clonedRequest = req;
-
-  if (storageService.isLoggedIn()) {
+  
+  if (storageService.isLoggedIn() && req.url.startsWith(environment.apiBaseUrl)) {
     const user = storageService.getUser();
     const token = user.token;
+
     if (token) {
-      clonedRequest = req.clone({
-        headers: req.headers.set('Authorization', 'Bearer ' + token)
+
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
       });
+      return next(authReq);
     }
   }
-  return next(clonedRequest);
+
+  return next(req);
 };
