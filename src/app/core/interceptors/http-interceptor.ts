927071dc5267f30848f24f3f800dde1787f4abfe
Steven Lazaro <sslc.247@gmail.com>
2025-07-19 19:33:05 -0500

    Cambio interceptor

--- src/app/core/interceptors/http-interceptor.ts
+++ src/app/core/interceptors/http-interceptor.ts
@@ -2,25 +2,29 @@ import { inject } from '@angular/core';
 import { HttpEvent, HttpInterceptorFn, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StorageService } from '../services/storage.service';
-import { environment } from '../../../environments/environment';
 
 export const HttpRequestInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
- 
+
+  console.log('--- Interceptor activado para la URL:', req.url); // LOG 1
+
   const storageService = inject(StorageService);
-  
-  if (storageService.isLoggedIn() && req.url.startsWith(environment.apiBaseUrl)) {
+
+  if (storageService.isLoggedIn()) {
+    console.log('--- Usuario está logueado, buscando token...'); // LOG 2
     const user = storageService.getUser();
     const token = user.token;
 
     if (token) {
-
+      console.log('--- Token encontrado. Adjuntando encabezado...'); // LOG 3
       const authReq = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
+        headers: req.headers.set('Authorization', 'Bearer ' + token)
       });
       return next(authReq);
+    } else {
+      console.log('--- ERROR: Usuario logueado pero no se encontró token.'); // LOG 4
     }
+  } else {
+    console.log('--- ADVERTENCIA: Usuario no está logueado. Petición sin token.'); // LOG 5
   }
 
   return next(req);
